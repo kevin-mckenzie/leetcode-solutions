b7701a6308709e44232a7d778aaaccbb392eb918
kbm967 <kevin.mckenzie@utexas.edu>
2022-09-30 09:03:11 -0400

    1

---
+++ arrays-hashing/two-sum-easy.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        index_map = {}
+        for i, n in enumerate(nums):
+            if target - n in index_map:
+                return [i, index_map[target - n]]
+            index_map[n] = i

4dda09a11a3efda3a02f8a5fd0b3480123306363
kbm967 <kevin.mckenzie@utexas.edu>
2022-10-17 08:39:18 -0400

    125

---
+++ two-pointers/is-palindrome-easy.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        lo = 0
+        hi = len(s) - 1
+        while hi > lo:
+            while (not s[lo].isalnum()) and (lo < hi):
+                lo += 1
+            while (not s[hi].isalnum()) and (hi > lo):
+                hi -= 1
+            if s[lo].lower() != s[hi].lower():
+                return False
+            lo += 1
+            hi -= 1
+            
+        return True
+        
